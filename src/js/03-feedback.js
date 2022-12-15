da2d7c1297661e335681b3ffb1310b5df34ef69f
feoalexei <feoalexei@ukr.net>
2022-12-15 19:24:06 +0200

    test.js added

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,12 @@
+const form = document.querySelector('.feedback-form');
 
+form.style.backgroundColor = 'tomato';
+
+form.addEventListener('submit', onSubmit);
+
+console.log('someting from 03-feedback.js');
+
+function onSubmit(e) {
+  e.preventDefault();
+  console.log('submit');
+}
---
+++ src/js/03-test.js
@@ -0,0 +1,5 @@
+// console.log('something from test');
+
+// const form = document.querySelector('.feedback-form');
+
+// form.style.backgroundColor = 'tomato';

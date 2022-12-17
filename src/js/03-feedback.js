351ba89fae078b1fed4ad1436c18edcef08292de
feoalexei <feoalexei@ukr.net>
2022-12-17 03:24:32 +0200

    task 3 fixed

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,34 +1,36 @@
 import throttle from 'lodash.throttle';
+import { save, load } from './storage.js';
 
 const form = document.querySelector('.feedback-form');
-const email = form.querySelector('input');
-const message = form.querySelector('textarea');
 const LOCAL_STORAGE_KEY = 'feedback-form-state';
 
 loadFromStorage();
 
-form.addEventListener('submit', throttle(submitFeedback, 500));
+form.addEventListener('input', throttle(inputFeedback, 500));
+form.addEventListener('submit', submitFeedback);
+
+function loadFromStorage() {
+  const fromStorage = load(LOCAL_STORAGE_KEY);
+
+  if (fromStorage) {
+    form.elements.email.value = fromStorage.email;
+    form.elements.message.value = fromStorage.message;
+  }
+}
+
+function createFeedbackObj() {
+  email = form.elements.email.value;
+  message = form.elements.message.value;
+  return { email, message };
+}
+
+function inputFeedback() {
+  save(LOCAL_STORAGE_KEY, createFeedbackObj());
+}
 
 function submitFeedback(e) {
   e.preventDefault();
-  const feedback = {};
-  feedback.email = e.target.elements.email.value;
-  feedback.message = e.target.elements.message.value;
-  if (feedback.email && feedback.message) {
-    localStorage.removeItem(LOCAL_STORAGE_KEY);
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(feedback));
-  }
+  localStorage.removeItem(LOCAL_STORAGE_KEY);
+  console.log(createFeedbackObj());
   e.currentTarget.reset();
-  console.log(feedback);
-}
-
-function loadFromStorage() {
-  const feedbackFromStorage = JSON.parse(
-    localStorage.getItem(LOCAL_STORAGE_KEY)
-  );
-
-  if (feedbackFromStorage) {
-    email.value = feedbackFromStorage.email;
-    message.value = feedbackFromStorage.message;
-  }
 }
